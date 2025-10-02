8d2ee45b692b35ed90bd500f55786188f028e0ef
Kushan Randika Herath <k14922160@gmail.com>
2025-10-02 18:15:15 +0530

    make Login Menu and Data Base
    Created New Database Connection Class

---
+++ Pos System/Pos System/DataBaseConnection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos_System
+{
+    public class DataBaseConnection
+    {
+        private string _connectionString;
+
+        public DataBaseConnection()
+        {
+            _connectionString = "Server=localhost;Database=POSSystemDB;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=true;";
+
+        }
+        
+        private SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection( _connectionString );
+
+        }
+
+        //Read Data For SQLDB
+
+        public DataTable ExaecuteQuary(string sql)
+        {
+            var connection = GetSqlConnection();
+
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(sql,connection);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+
+        }
+
+        // ADD UPDATA DELETE Data For SQLDB
+        public int ExecuteNonQuary(string sql)
+        {
+            var connection = GetSqlConnection();
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            return cmd.ExecuteNonQuery();
+
+        }
+
+
+
+    }
+}
