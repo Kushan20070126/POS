2fc4dec4c98f03e7cd6eafd5d43c20451fbcb510
Kushan Randika Herath <k14922160@gmail.com>
2025-10-02 17:02:56 +0530

    make the dbacesslayer_dbconnection funtions

---
+++ Pos System/Pos System/dbacesslayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos_System
+{
+    internal class dbacesslayer
+    {
+
+        public void dbacesslayer_dbconnection()
+        {
+
+        }
+    }
+}
